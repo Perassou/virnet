21ac0fbb402952cea4070c1bead8122650c524a3
Marie Peras <marieperas@Maries-MacBook-Pro.local>
2021-07-12 08:10:19 -0700

    updates to make the code works

--- AttentionLayer.py
+++ AttentionLayer.py
@@ -1,5 +1,5 @@
 from keras import initializers
-from keras.engine import InputSpec, Layer
+from keras.layers import InputSpec, Layer
 from keras import backend as K
 
 
@@ -22,7 +22,7 @@ class AttentionWeightedAverage(Layer):
         self.W = self.add_weight(shape=(input_shape[2], 1),
                                  name='{}_W'.format(self.name),
                                  initializer=self.init)
-        self.trainable_weights = [self.W]
+        self._trainable_weights = [self.W]
         super(AttentionWeightedAverage, self).build(input_shape)
 
     def call(self, x, mask=None):
